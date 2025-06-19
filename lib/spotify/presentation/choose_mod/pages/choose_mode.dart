8931c964246261334e1cc79d6aebb8b3e33e1ceb
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2025-06-20 00:13:59 +0300

    bhhtr

--- lib/spotify/presentation/choose_mod/pages/choose_mode.dart
+++ lib/spotify/presentation/choose_mod/pages/choose_mode.dart
@@ -1,30 +1,30 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
-import 'package:spotify_clone/core/configs/assets/app_images.dart';
-import 'package:spotify_clone/core/configs/assets/app_vectors.dart';
-import 'package:spotify_clone/core/configs/theme/app_colors.dart';
-import 'package:spotify_clone/spotify/presentation/choose_mod/pages/choose_mode.dart';
+import 'package:flutter_svg/svg.dart';
 
-class GetStartedPage extends StatelessWidget {
-  const GetStartedPage({super.key});
+import '../../../../common/widgets/button/basic_app_button.dart';
+import '../../../../core/configs/assets/app_images.dart';
+import '../../../../core/configs/assets/app_vectors.dart';
+import '../../../../core/configs/theme/app_colors.dart';
+
+class ChooseModePage extends StatelessWidget {
+  const ChooseModePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Stack(
+      body: Stack(
         children: [
           Container(
             padding: const EdgeInsets.symmetric(
-              vertical: 40,
-              horizontal: 40
+                vertical: 40,
+                horizontal: 40
             ),
             decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                  image: AssetImage(AppImages.introBG))
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(AppImages.chooseModeBG))
             ),
           ),
           Container(
@@ -33,14 +33,14 @@ class GetStartedPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(
                 vertical: 40,
-            horizontal:40 ),
+                horizontal:40 ),
             child: Column(
               children: [
                 Align(
                     alignment: Alignment.topCenter,
                     child: SvgPicture.asset(AppVectors.logo)),
                 const Spacer(),
-                const Text('Enjoy Listening To Music',
+                const Text('Choose Mode',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
@@ -53,7 +53,7 @@ class GetStartedPage extends StatelessWidget {
                   fontSize: 13,
                 ),
                   textAlign: TextAlign.center,),
-                const SizedBox(height: 20,),
+                const SizedBox(height: 50,),
                 BasicAppButton(
                     onPressed: (){
                       Navigator.push(context, MaterialPageRoute(builder: (c)=>ChooseModePage()));
@@ -66,6 +66,7 @@ class GetStartedPage extends StatelessWidget {
 
         ],
       ),
+
     );
   }
 }
